e3735e182ec68632b10cf78264aee1629b5a3154
Evzen <thezentan@gmail.com>
2022-12-08 18:52:43 +0800

    Completed status of hangman

--- main.py
+++ main.py
@@ -4,97 +4,286 @@ st.title("Hangman")
 
 if "count" not in st.session_state:
     st.session_state.count = 0
-def failed_attempts():
-    st.session_state.count+=1
 
 with st.container():
     if st.session_state.count == 1:
         st.code('''
-______________
+_________________
+|________________|
 ''')
+
     if st.session_state.count == 2:
         st.code('''
-Be careful!
-
-       |
-       |
-       |
-       |
-       |
-       |
-       |
-       |
-       |
-       |
-_______|________
+Try again...
+       ___
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | | 
+_______|_|_______
+|________________|
 ''')
+
     if st.session_state.count == 3:
         st.code('''
-Are you sure about that?
-
-       |-----------------
-       |
-       |
-       |
-       |
-       |
-       |
-       |
-       |
-       |
-_______|________
+Try again...
+       _____________________
+       | .__________________|
+       | | / /      
+       | |/ /
+       | | /
+       | |/
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | | 
+_______|_|_______
+|________________|
 ''')
     if st.session_state.count == 4:
         st.code('''
-You are dogwater...
-
-       |-----------------
-       |                |
-       |
-       |
-       |
-       |
-       |
-       |
-       |
-       |
-_______|________
+Try again...
+       ___________..________
+       | .__________))______|
+       | | / /      ||
+       | |/ /
+       | | /
+       | |/
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | | 
+_______|_|_______
+|________________|
 ''')
+
+    if st.session_state.count == 5:
+        st.code('''
+Try again...
+       ___________..________
+       | .__________))______|
+       | | / /      ||
+       | |/ /       ||
+       | | /        || .-''.
+       | |/         ||/     |
+       | |          |||     |
+       | |           \\\ __ /
+       | |         .-`--'.
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | |
+       | | 
+_______|_|_______
+|________________|
+''')
+
+    if st.session_state.count == 6:
+        st.code('''
+Try again...
+       ___________..________
+       | .__________))______|
+       | | / /      ||
+       | |/ /       ||
+       | | /        || .-''.
+       | |/         ||/     |
+       | |          |||     |
+       | |           \\\ __ /
+       | |          .-`--'.
+       | |          \ . . /
+       | |           |   |
+       | |           | . |
+       | |           |___|
+       | |
+       | |
+       | |
+       | | 
+_______|_|_______
+|________________|
+''')
+
+    if st.session_state.count == 7:
+        st.code('''
+Try again...
+       ___________..________
+       | .__________))______|
+       | | / /      ||
+       | |/ /       ||
+       | | /        || .-''.
+       | |/         ||/     |
+       | |          |||     |
+       | |           \\\ __ /
+       | |          .-`--'.
+       | |        //\ . . /
+       | |       //  |   |
+       | |      //   | . |
+       | |     ('    |___|
+       | |
+       | |
+       | |
+       | | 
+_______|_|_______
+|________________|
+''')
+
+    if st.session_state.count == 8:
+        st.code('''
+Try again...
+       ___________..________
+       | .__________))______|
+       | | / /      ||
+       | |/ /       ||
+       | | /        || .-''.
+       | |/         ||/     |
+       | |          |||     |
+       | |           \\\ __ /
+       | |          .-`--'.
+       | |        //\ . . /\\
+       | |       //  |   |  \\
+       | |      //   | . |   \\
+       | |     ('    |___|    ')
+       | |
+       | |
+       | |
+       | | 
+_______|_|_______
+|________________|
+''')
+
+    if st.session_state.count == 9:
+        st.code('''
+Try again...
+       ___________..________
+       | .__________))______|
+       | | / /      ||
+       | |/ /       ||
+       | | /        || .-''.
+       | |/         ||/     |
+       | |          |||     |
+       | |           \\\ __ /
+       | |          .-`--'.
+       | |        //\ . . /\\
+       | |       //  |   |  \\
+       | |      //   | . |   \\
+       | |     ('    |___|    ')
+       | |           ||
+       | |           ||
+       | |           ||
+       |_|          <_|
+_______|_|_______
+|________________|
+''')
+
+    if st.session_state.count == 10:
+        st.code('''
+He died...
+       ___________..________
+       | .__________))______|
+       | | / /      ||
+       | |/ /       ||
+       | | /        || .-''.
+       | |/         ||/     |
+       | |          |||     |
+       | |           \\\ __ /
+       | |          .-`--'.
+       | |        //\ . . /\\
+       | |       //  |   |  \\
+       | |      //   | . |   \\
+       | |     ('    |___|    ')
+       | |           || ||
+       | |           || ||
+       | |           || ||
+       | |          <_| |_>
+_______|_|_______
+|________________|
+''')
+
+word = "Deez nuts"
+UC_word = word.upper()
+listed_word = list(UC_word)
+
+# len_word = len(word)
+# for i in range(len_word):
+#     st.markdown("_")
+
+def correct_or_not(letter):
+    b_status = st.button(str(letter))
+    if b_status == True:
+        if letter not in listed_word:
+            st.session_state.count+=1
+        elif letter in listed_word:
+            correct_letters = letter
+            listed_word.remove(letter)
+            if listed_word == None:
+                st.success(':white_check_mark: You have saved the man')
 
 kpi1, kpi2, kpi3, kpi4, kpi5, kpi6, kpi7, kpi8, kpi9 = st.columns(9)
 with kpi1:
-    A_button = st.button("A", disabled=1)
-    J_button = st.button("J", disabled=1)
-    S_button = st.button("S", disabled=1)
+    correct_or_not("A")
+    correct_or_not("J")
+    correct_or_not("S")
 with kpi2:
-    B_button = st.button("B", disabled=1)
-    K_button = st.button("K", disabled=1)
-    T_button = st.button("T", disabled=1)
+    correct_or_not("B")
+    correct_or_not("K")
+    correct_or_not("T")
 with kpi3:
-    C_button = st.button("C", disabled=1)
-    L_button = st.button("L", disabled=1)
-    U_button = st.button("U", disabled=1)
+    correct_or_not("C")
+    correct_or_not("L")
+    correct_or_not("U")
 with kpi4:
-    D_button = st.button("D", disabled=1)
-    M_button = st.button("M", disabled=1)
-    V_button = st.button("V", disabled=1)
+    correct_or_not("D")
+    correct_or_not("M")
+    correct_or_not("V")
 with kpi5:
-    E_button = st.button("E", disabled=1)
-    N_button = st.button("N", disabled=1)
-    W_button = st.button("W", disabled=1)
+    correct_or_not("E")
+    correct_or_not("N")
+    correct_or_not("W")
 with kpi6:
-    F_button = st.button("F", disabled=1)
-    O_button = st.button("O", disabled=1)
-    X_button = st.button("X", disabled=1)
+    correct_or_not("F")
+    correct_or_not("O")
+    correct_or_not("X")
 with kpi7:
-    G_button = st.button("G", disabled=1)
-    P_button = st.button("P", disabled=1)
-    Y_button = st.button("Y", disabled=1)
+    correct_or_not("G")
+    correct_or_not("P")
+    correct_or_not("Y")
 with kpi8:
-    H_button = st.button("H", disabled=1)
-    Q_button = st.button("q", disabled=1)
-    Z_button = st.button("Z", disabled=1)
+    correct_or_not("H")
+    correct_or_not("Q")
+    correct_or_not("Z")
 with kpi9:
-    I_button = st.button("I", disabled=1)
-    R_button = st.button("R", disabled=1)
-button = st.button("Hi", on_click=failed_attempts)
+    correct_or_not("I")
+    correct_or_not("R")
+
+if st.session_state.count > 10:
+    retry_button = st.button("Retry")
+    if retry_button:
+        st.session_state.count = 0
